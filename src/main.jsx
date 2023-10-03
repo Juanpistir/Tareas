f82d7aebcfcec17b81402b9858d97a2d4df99d4c
juanpistir <84823227+Juanpistir@users.noreply.github.com>
2023-10-03 04:11:07 -0500

    fallo arreglado

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import "./index.css";
 import { TaskContextProvider } from "../src/context/TaskContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <main className="bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% h-screen">
+  <main className="bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% min-h-screen">
     <div className="container mx-auto">
       <React.StrictMode>
         <TaskContextProvider>
