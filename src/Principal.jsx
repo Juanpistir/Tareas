d6515f6d188e9214ba2b681f76639c189a4be5f1
juanpistir <84823227+Juanpistir@users.noreply.github.com>
2023-10-03 03:58:55 -0500

    intentando arreglar un fallo

--- src/Principal.jsx
+++ src/Principal.jsx
@@ -3,12 +3,10 @@ import Tasklist from "./components/Tasklist";
 
 function Principal() {
   return (
-    <main className="bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% h-screen">
-      <div className="container mx-auto">
-        <Taskform />
-        <Tasklist />
-      </div>
-    </main>
+    <>
+      <Taskform />
+      <Tasklist />
+    </>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,13 @@ import "./index.css";
 import { TaskContextProvider } from "../src/context/TaskContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <TaskContextProvider>
-      <Principal />
-    </TaskContextProvider>
-  </React.StrictMode>
+  <main className="bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% h-screen">
+    <div className="container mx-auto">
+      <React.StrictMode>
+        <TaskContextProvider>
+          <Principal />
+        </TaskContextProvider>
+      </React.StrictMode>
+    </div>
+  </main>
 );
